c83ba3018b2a48ea2c8bd5396e89ea644878f00c
kietelmuis <omar.hopman9@gmail.com>
2026-01-25 20:29:37 +0100

    base webapi

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer};
+
+use crate::api::web::health;
 
 mod api;
 
@@ -9,7 +11,7 @@ const PORT: u32 = 9000;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     println!("[webapi] running at port {}", PORT);
-    HttpServer::new(|| App::new().route("/", web::get()))
+    HttpServer::new(|| App::new().service(health))
         .bind(format!("[::1]:{}", PORT))?
         .run()
         .await?;
