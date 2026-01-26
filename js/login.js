5850a1862042766befb1aabf1bb02452c2886d81
kietelmuis <omar.hopman9@gmail.com>
2026-01-26 14:35:32 +0100

    fixup

--- js/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    let butt = document.getElementById("login");
-    butt.onclick = () => {
-        console.log("Starting NiggaLink");
-    };
-});
