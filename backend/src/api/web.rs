5b4a952a3347b32e4202e464406cd9c983ad5f49
kietelmuis <omar.hopman9@gmail.com>
2026-01-26 19:47:58 +0100

    game methods

--- backend/src/api/web.rs
+++ backend/src/api/web.rs
@@ -1,4 +1,57 @@
-use actix_web::{HttpResponse, Responder, get};
+use actix_ws::AggregatedMessage;
+use actix_web::web::{Data, Json, Path, Payload};
+use actix_web::{get, rt, Error, HttpRequest, HttpResponse, Responder};
+use futures_util::StreamExt;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use crate::api::web;
+use crate::AppState;
+
+#[derive(Deserialize, Serialize)]
+struct GameResponse {
+	game_id: Uuid
+}
+
+// main websocket
+pub async fn game_get(data: Data<AppState>, req: HttpRequest, stream: Payload) -> Result<HttpResponse, Error> {
+	let (res, mut session, stream) = actix_ws::handle(&req, stream)?;
+
+    let mut stream = stream
+        .aggregate_continuations()
+        .max_continuation_size(2_usize.pow(20));
+
+    rt::spawn(async move {
+        while let Some(msg) = stream.next().await {
+            match msg {
+                Ok(AggregatedMessage::Ping(msg)) => {
+                    session.pong(&msg).await.unwrap();
+                }
+                _ => {}
+            }
+        }
+    });
+
+    Ok(res)
+}
+
+#[get("/game/start/{uuid}")]
+async fn game_start(data: Data<AppState>, path: Path<String>) -> Result<impl Responder, Error> {
+	println!("starting game {}", path.into_inner());
+	Ok(HttpResponse::Ok())
+}
+
+#[get("/game/join/{uuid}")]
+async fn game_join(data: Data<AppState>, path: Path<String>) -> Result<impl Responder, Error> {
+	println!("joining game {}", path.into_inner());
+	Ok(HttpResponse::Ok())
+}
+
+#[get("/game/create")]
+async fn game_create() -> Result<impl Responder, Error> {
+	println!("creating game");
+	Ok(web::Json(GameResponse { game_id: Uuid::new_v4() }))
+}
 
 #[get("/health")]
 async fn health() -> impl Responder {
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,55 @@
-use std::error::Error;
+use std::{collections::{HashMap, VecDeque}, error};
 
-use actix_web::{App, HttpServer};
+use actix_web::{get, web::{get, Data, Form}, App, HttpResponse, HttpServer, Responder};
+use parking_lot::Mutex;
+use uuid::Uuid;
 
-use crate::api::web::health;
+use crate::api::{tiktok::TiktokApi, web::{game_create, game_get, game_join, game_start, health}};
 
 mod api;
 
 const PORT: u32 = 9000;
 
+struct UserState {
+    videos: VecDeque<String>,
+    tiktok_api: TiktokApi,
+}
+
+struct AppState {
+    users: Mutex<HashMap<String, UserState>>,
+}
+
+#[derive(Deserialize)]
+struct AuthCallback {
+	code: String
+}
+
+#[get("/auth/callback")]
+async fn auth_callback(state: Data<AppState>, form: Form<AuthCallback>) -> impl Responder {
+    let token = form.into_inner().code;
+
+    {
+        let mut users = state.users.lock();
+        users.insert(
+            Uuid::new_v4().to_string(),
+            UserState {
+                tiktok_api: TiktokApi::new(token),
+                videos: VecDeque::new(),
+            },
+        );
+    }
+
+    HttpResponse::Ok()
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn error::Error>> {
+	let state = Data::new(AppState {
+		users: Mutex::new(HashMap::new())
+	});
+	
     println!("[webapi] running at port {}", PORT);
-    HttpServer::new(|| App::new().service(health))
+    HttpServer::new(move || App::new().app_data(state.clone()).service(health).service(auth_callback).service(game_create).service(game_join).service(game_start).route("/game", get().to(game_get)))
         .bind(format!("0.0.0.0:{}", PORT))?
         .run()
         .await?;
